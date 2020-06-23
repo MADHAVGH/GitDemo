d28b52051f92da4b959e24c60c1f93b9f45ff1b5
MADHAVGH <madhavreddy543@yahoo.in>
2020-06-23 18:42:56 +0530

    My first commit

---
+++ src/test/java/mqaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,18 @@
+package mqaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void nativeapp()
+	{
+		System.out.println("nativeapp");
+	}
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+
+}
